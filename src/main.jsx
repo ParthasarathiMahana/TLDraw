53edbc02deccd641590c52a64e9603d4d2b326ad
Parthasarathi Mahana <parthasarathimahana@Parthasarathis-MacBook-Air.local>
2024-03-19 15:41:43 +0530

    tldraw complete.

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { Tldraw } from 'tldraw'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div style={{ position: 'fixed', inset: 0 }}>
+      <Tldraw />
+    </div>
+  </React.StrictMode>,
+)
